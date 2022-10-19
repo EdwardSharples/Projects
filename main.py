f25b92e0323a154c15635044d26ee6abf1415cb2
SharplesPepe <116102362+SharplesPepe@users.noreply.github.com>
2022-10-19 12:24:20 +0100

    Control the cube!
    Added the ability to control the now rectangular blue cube to go in all directions and rotate.

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from ursina import *
 
 app = Ursina()
-cube = Entity(model="cube", coplor=color.red, textrure="white_cube", scale=2)
+player = Entity(model="cube", color=color.blue, scale_y=2)
 
 def update():
-    cube.rotation_x = cube.rotation_x + 0.25
-    cube.rotation_y = cube.rotation_y + 0.5
+    player.x += held_keys["d"] * 0.1
+    player.x -= held_keys["a"] * 0.1
+    player.y += held_keys["w"] * 0.1
+    player.y -= held_keys["s"] * 0.1
+    player.rotation_x += held_keys["r"] * 5
+    player.rotation_y += held_keys["r"] * 5
 
 app.run()
